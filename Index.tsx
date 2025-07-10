aa63a18e7bb5e482b48f33208c98ecd05bd2c7b7
Romeo Maunick <rmaunick26@gmail.com>
2025-07-10 16:33:36 +0100

    SRC - Pages

---
+++ Index.tsx
@@ -0,0 +1,14 @@
+
+import { EncryptionTool } from '@/components/EncryptionTool';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-50 via-blue-50 to-indigo-50">
+      <div className="container mx-auto py-8">
+        <EncryptionTool />
+      </div>
+    </div>
+  );
+};
+
+export default Index;
